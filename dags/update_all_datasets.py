debce422ec4f363ad637ee6d17907dfbd083237c
semio <prairy.long@gmail.com>
2017-12-28 20:56:41 +0800

    correct datetime

--- dags/update_all_datasets.py
+++ dags/update_all_datasets.py
@@ -151,7 +151,8 @@ def refresh_dags(**context):
         dag_name = dataset.replace('/', '_')
         dag_path = osp.join(airflow_home, 'dags', dag_name)
         now = datetime.utcnow() - timedelta(days=1) + timedelta(minutes=10)
-        dt = 'datetime({}, {}, {})'.format(now.year, now.month, now.day)
+        dt = 'datetime({}, {}, {}, {}, {})'.format(now.year, now.month,
+                                                   now.day, now.hour, now.minute)
         with open(dag_path+'.py', 'w') as f:
             f.write(template.render(name=dag_name,
                                     datetime=dt,
