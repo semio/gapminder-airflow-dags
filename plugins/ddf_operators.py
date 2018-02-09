a48519146e5d4f463ddbca9ffd81484ad176aa21
semio <prairy.long@gmail.com>
2018-02-09 17:41:46 +0800

    do not include datapackage in diff list

--- plugins/ddf_operators.py
+++ plugins/ddf_operators.py
@@ -113,7 +113,7 @@ class GitPushOperator(BashOperator):
             git push -u origin
         else
             HAS_UPDATE=0
-            for f in $(git diff --name-only); do
+            for f in $(git diff --name-only | grep -v datapackage.json); do
                 if [[ $(git diff $f | tail -n +5 | grep -e "^[++|\-\-]" | head -c1 | wc -c) -ne 0 ]]; then
                     HAS_UPDATE=1
                     git add $f
