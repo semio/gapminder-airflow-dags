9caeaea10c613916ace24dc68e9464f4c4c568ca
semio <prairy.long@gmail.com>
2018-02-11 10:29:14 +0800

    don't include warning tag

--- plugins/ddf_operators.py
+++ plugins/ddf_operators.py
@@ -139,7 +139,7 @@ class ValidateDatasetOperator(BashOperator):
         bash_command = '''\
         set -eu
         cd {{ params.dataset }}
-        validate-ddf ./
+        validate-ddf ./ --exclude-tags "WARNING"
         if [ `ls | grep validation*.log | wc -c` -ne 0 ]
         then
             mv validation*.log {{ params.logpath }}
