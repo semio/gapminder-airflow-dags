5bf07f2a83ec3206d9dcc26a1ac7a9dfe4220e17
semio <prairy.long@gmail.com>
2018-05-05 11:25:24 +0800

    handle find not found error

--- dags/update_all_datasets.py
+++ dags/update_all_datasets.py
@@ -133,8 +133,11 @@ def _get_dataset_type(dataset):
     dataset_path = osp.join(datasets_dir, dataset)
     etl_dir = osp.join(dataset_path, 'etl/scripts')
 
-    out = subprocess.run(['ddf', 'etl_type', '-d', etl_dir], 
-                         stdout=subprocess.PIPE).stdout.decode('utf-8')
+    try:
+        out = subprocess.run(['ddf', 'etl_type', '-d', etl_dir],
+                             stdout=subprocess.PIPE).stdout.decode('utf-8')
+    except FileNotFoundError:
+        return ['manual', '']
     return out.split(',')
 
 
