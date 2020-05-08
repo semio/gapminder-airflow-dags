6168ea8fe93f0c46753ec41dcb4558aab68e8e41
semio <semio@posteo.net>
2020-05-08 15:45:24 +0800

    fix bash

--- plugins/ddf_operators.py
+++ plugins/ddf_operators.py
@@ -210,7 +210,7 @@ class ValidateDatasetOperator(BashOperator):
                     mkdir $LOGPATH
                 fi
                 mv $VALIDATE_OUTPUT $LOGPATH
-                LOGURL = "http://159.89.2.170:8081/`basename {{ params.dataset }}`/$VALIDATE_OUTPUT"
+                LOGURL="http://159.89.2.170:8081/`basename {{ params.dataset }}`/$VALIDATE_OUTPUT"
                 echo "you can access the log at $LOGURL"
                 exit 1
             else
