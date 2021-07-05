4d5bb1dec28d31bf9b7d5bc7220d041ce4759784
semio <semio@posteo.net>
2021-07-05 14:34:52 +0800

    correct command

--- plugins/ddf_operators.py
+++ plugins/ddf_operators.py
@@ -98,7 +98,7 @@ class GitPullOperator(BashOperator):
         set -eu
         cd {{ params.dataset }}
         git pull
-        git submodule foreach git pull
+        git submodule foreach git pull origin master
         '''
         super().__init__(bash_command=bash_command,
                          params={'dataset': dataset},
